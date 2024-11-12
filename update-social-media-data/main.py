8be2678d4f89d38959d6c6b1f7167097e67a5534
Anmol <anmolsaxena456@gmail.com>
2024-11-11 23:13:17 -0600

    complete metric updates
    i'll run the python script locally via a cron job, that'll keep everything up to date. and i need to create the memo sheet. other than that, everything else is done!

---
+++ update-social-media-data/main.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from get_data_points import get_data_points
+import psycopg
+import os
+import json
+
+def update_db(all_time_reach: int, yesterday_reach: int, all_time_views: int) -> None:
+	curr_metrics = {}
+	
+	with psycopg.connect(str(os.getenv('POSTGRES_URL'))) as conn:
+		with conn.cursor() as cursor:
+			cursor.execute('SELECT * FROM metrics_db WHERE type != \'Last 30 Minutes\'')
+			
+			for row in cursor.fetchall():
+				curr_metrics[row[0]] = json.loads(row[1])
+	
+	curr_metrics['Yesterday']['data_points']['reach'] = yesterday_reach
+	curr_metrics['Yesterday']['data_points']['views'] = all_time_views - curr_metrics['All Time']['data_points']['views']
+	
+	curr_metrics['All Time']['data_points']['reach'] = all_time_reach
+	curr_metrics['All Time']['data_points']['views'] = all_time_views
+	
+	curr_metrics['Yesterday']['metrics']['uctr'] = round((curr_metrics['Yesterday']['data_points']['visitors'] / curr_metrics['Yesterday']['data_points']['reach']) * 100)
+	curr_metrics['Yesterday']['metrics']['gctr'] = round((curr_metrics['Yesterday']['metrics']['visits'] / curr_metrics['Yesterday']['data_points']['views']) * 100)
+	
+	curr_metrics['All Time']['metrics']['uctr'] = round((curr_metrics['All Time']['data_points']['visitors'] / curr_metrics['All Time']['data_points']['reach']) * 100)
+	curr_metrics['All Time']['metrics']['gctr'] = round((curr_metrics['All Time']['metrics']['visits'] / curr_metrics['All Time']['data_points']['views']) * 100)
+	
+	with psycopg.connect(str(os.getenv('POSTGRES_URL'))) as conn:
+		with conn.cursor() as cursor:
+			query = "UPDATE metrics_db SET data = %s::jsonb WHERE type = %s"
+			
+			cursor.execute(query, (json.dumps(curr_metrics["Yesterday"]), 'Yesterday'))
+			cursor.execute(query, (json.dumps(curr_metrics["All Time"]), 'All Time'))
+			
+			conn.commit()
+
+def main() -> None:
+	load_dotenv()
+	
+	all_time_reach, yesterday_reach, all_time_views = get_data_points()
+	
+	update_db(all_time_reach, yesterday_reach, all_time_views)
+
+if __name__ == '__main__':
+	main()
